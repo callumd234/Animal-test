34d4fb683a1afe1ed27e4021e4ad3deafa83b49d
callumd234 <duffieldcallum123@gmail.com>
2022-01-25 11:39:42 +0000

    added more tests and more methods to the cat class

--- src/main/java/com/tsi/duff/callum/Cat.java
+++ src/main/java/com/tsi/duff/callum/Cat.java
@@ -3,6 +3,7 @@ public class Cat extends com.tsi.duff.callum.Mammal {
 
     private int age;
     private String awake;
+    private String sleep;
 
     public void setCatAge(int age){
         this.age = age;
@@ -10,5 +11,13 @@ public class Cat extends com.tsi.duff.callum.Mammal {
 
     public int getCatAge(){
         return age;
+    }
+
+    public String setSleep(){
+        return "I am asleep";
+    }
+
+    public String whatTheCatisEating(){
+        return "I am eating tuna";
     }
     }
--- src/test/java/com/tsi/duff/callum/test/AnimalTest.java
+++ src/test/java/com/tsi/duff/callum/test/AnimalTest.java
@@ -35,10 +35,24 @@ public class AnimalTest {
         }
 
         @Test
-    public void test_sleeping(){
+    public void test_cat_age(){
         Cat cat1 = new Cat();
         cat1.setCatAge(10);
         assertEquals(10, cat1.getCatAge(),"the cat is not 10");
+        }
+
+        @Test
+    public void test_cat_sleeping(){
+        Cat cat2 = new Cat();
+        cat2.setSleep();
+        assertEquals("I am asleep", cat2.setSleep(), "the cat must be awake");
+        }
+
+        @Test
+    public void test_cat_eats(){
+        Cat cat3 = new Cat();
+        cat3.whatTheCatisEating();
+        assertEquals("I am eating tuna", cat3.whatTheCatisEating(), "The cat is not eating Tuna");
         }
 
     }
