ff7c12f7917547bb09eefdf79a2712aba2fb32d8
callumd234 <duffieldcallum123@gmail.com>
2022-01-25 14:01:23 +0000

    changed the code and added tests together

--- src/test/java/com/tsi/duff/callum/test/AnimalTest.java
+++ src/test/java/com/tsi/duff/callum/test/AnimalTest.java
@@ -1,5 +1,7 @@
 package com.tsi.duff.callum.test;
 import com.tsi.duff.callum.Animal;
+import com.tsi.duff.callum.Cat;
+import com.tsi.duff.callum.FlyingRat;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -32,6 +34,48 @@ public class AnimalTest {
         testAnimal2.setName("Jermain");
         assertEquals("Jermain", testAnimal2.getName(), "the name is wrong");
         }
+
+    @Test
+    void test_flyingrat_takeoff(){
+        FlyingRat flyingrat = new FlyingRat();
+        flyingrat.takeOffMethod();
+        assertEquals(flyingrat.getTakeOffMethod(), "flap wings and move legs to start flying", "take off method is wrong");
+    }
+
+    @Test
+    void test_flyingrat_flying(){
+        FlyingRat flyingrat1 = new FlyingRat();
+        flyingrat1.flying();
+        assertEquals(flyingrat1.getFlying(), "keep flapping wings and glide through the air", "flying method is wrong");
+    }
+
+    @Test
+    void test_flyingrat_touchdown(){
+        FlyingRat flyingrat2 = new FlyingRat();
+        flyingrat2.touchdown();
+        assertEquals(flyingrat2.getTouchdown(), "stop flapping wings and land gently", "touchdown method is wrong");
+    }
+
+    @Test
+    void test_cat_age(){
+        Cat cat1 = new Cat();
+        cat1.setCatAge(10);
+        assertEquals(10, cat1.getCatAge(),"the cat is not 10");
+    }
+
+    @Test
+    void test_cat_sleeping(){
+        Cat cat2 = new Cat();
+        cat2.setSleep();
+        assertEquals("I am asleep", cat2.setSleep(), "the cat must be awake");
+    }
+
+    @Test
+    void test_cat_eats(){
+        Cat cat3 = new Cat();
+        cat3.whatTheCatisEating();
+        assertEquals("I am eating tuna", cat3.whatTheCatisEating(), "The cat is not eating Tuna");
+    }
 
     }
 
